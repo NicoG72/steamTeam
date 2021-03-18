2f458e2aa9838f47768a637b2c5b208a4625666d
Carolina Defranceschi <karodefranceschi@gmail.com>
2021-03-18 17:01:37 -0300

    resolviendo conflictos en el admin

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,23 @@
 import {Producto} from './productClass.js'
+
+//variable global este arreglo tendra todos los productos que guardo en mi local storage
+let listaProductos = [];
+
+//funcion anonima
+window.agregarProducto = function(event){
+    event.preventDefault();
+
+
+    let codigo = document.getElementById('codigo').value;
+    let nombre = document.getElementById('nombreProducto').value;
+    let categoria = document.getElementById('categoria').value;
+    let destacar = document.getElementById('checkDestacar').value;
+    let publicar = document.getElementById('checkPublicar').value;
+
+    let nuevoProducto = new Producto (codigo, nombre, categoria, destacar, publicar);
+   
+    listaProductos.push(nuevoProducto);
+   
+    console.log(nuevoProducto)
+    
+}
--- js/productClass.js
+++ js/productClass.js
@@ -2,8 +2,8 @@ export class Producto {
     constructor (codigo,nombre,categoria,publicado,destacado){
     this.codigo = codigo;
     this.nombre = nombre;
-    this.categoria= categoria;
-    this.publicado=publicado;
-    this.destacado=destacado;
+    this.categoria = categoria;
+    this.publicado =publicado;
+    this.destacado = destacado;
 }
 }
