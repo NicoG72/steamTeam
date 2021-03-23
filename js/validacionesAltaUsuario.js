999ec7692761416b4790565318a3f31a15c189b5
Alejandro Poviña <alejandro.povina@gmail.com>
2021-03-23 10:50:02 -0300

    se agrego emailjs en la validacion de alta de usuarios y se modifico el mensaje de alerta

--- js/validacionesAltaUsuario.js
+++ js/validacionesAltaUsuario.js
@@ -43,9 +43,8 @@ window.validarEmail = function(email){
 
 //valido contraseña
 window.validarPassword = function (pass){
-    console.log(pass);
-    let expresion = /[a-z]+[0-9]/;
-    if(pass.value.trim()!="" && expresion.test(pass.value)){
+    console.log(pass) ;
+    if(pass.value.trim()!="" && expresion.test(pass.value) && pass.value.length >= 10){
         pass.className="form-control is-valid";
         return true;
     }else{
@@ -83,7 +82,6 @@ function usuarioUnico(){
                 return false;
             }   
         }
-    
 }   
 
 //creo la funcion validar general el nuevo usuasrio
@@ -128,16 +126,14 @@ window.crearNuevoUsuario= function (event){
 
         localStorage.setItem("listaUsuariosKey", JSON.stringify(listaUsuarios));
 
-        Swal.fire(
-            'Bienvenido',
-            'Su usuario ha sido creado correctamente',
-            'success'
-          )
-          leerDatosLS();
-          limpiarFormUsuario();
-          modalUsuario.hide();
+        enviarSolicitudAlta();
+        leerDatosLS();
+        limpiarFormUsuario();
+        modalUsuario.hide();
 
-    }else{console.log("nuevo usuario invalido");}
+    }else{
+        console.log("nuevo usuario invalido");
+}
 }
 
 export function leerDatosLS(){
@@ -147,6 +143,32 @@ export function leerDatosLS(){
             listaUsuarios = _listaUsuarios
         }
     }
+}
+
+
+function enviarSolicitudAlta() {
+    emailjs.send("service_nc6s2ua","template_2hgp0pb",{
+  from_name: `${document.getElementById('nombreUsuario').value}`,
+  to_name: "Steam Team",
+  Email: `Correo Electronico: ${document.getElementById('emailUsuario').value}`,
+  Consulta: `Solicitud de Usuario`,
+  }).then(function (response){
+      console.log(response);
+       Swal.fire(
+           
+           'Bienvenido',
+                   'Su usuario ha sido creado correctamente',
+                   'success'
+        );
+    }, function (error){
+        console.log(error)
+        Swal.fire({
+            icon: 'error',
+            title: 'Ups! Ocurrio un error',
+            text: 'Intentelo de nuevo mas tarde',
+            footer: '<a href>Why do I have this issue?</a>'
+          })
+  })
 }
 
 function limpiarFormUsuario(){
